a61011fd98bd30a0b38fac8b16d244c46c9414e0
Bhavesh-Flutter <bhavesh.gohil@lenditt.com>
2023-05-27 17:31:20 +0530

    added health check route

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ const connectWithDB = require("./db/db");
 
 const app = express();
 
+// Health check function
+app.get("/", (_, res) => {
+  res.send(`Server is listening at ${new Date()}`);
+});
+
 app.listen(config.PORT, () => {
   console.log(`server listening on http://localhost:${config.PORT}`);
   connectWithDB();
